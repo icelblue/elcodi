84d0b8e20fd57fe370fde53cbec7dc22c5d9ff0c
icelblue <maricel.alsina@dxc.com>
2024-09-01 10:19:56 +0200

    nous endpoints

--- server.js
+++ server.js
@@ -29,6 +29,45 @@ const rankingSchema = new mongoose.Schema({
 
 const Ranking = mongoose.model('Ranking', rankingSchema);
 
+// Endpoint para la ruta raíz
+app.get('/', (req, res) => {
+    res.send('Bienvenido a El Codi Game API. Utiliza los endpoints /ranking y /xp para obtener información.');
+});
+
+// Endpoint para obtener puntos XP
+app.get('/xp', async (req, res) => {
+    const date = req.query.date;
+    try {
+        const entry = await Ranking.findOne({ date: date });
+        if (entry) {
+            res.json({ xp: entry.xp });
+        } else {
+            res.json({ xp: 0 });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Endpoint para agregar puntos XP
+app.post('/xp', async (req, res) => {
+    const { xp, date } = req.body;
+    try {
+        const existingEntry = await Ranking.findOne({ date: date });
+        if (existingEntry) {
+            existingEntry.xp = Math.max(existingEntry.xp, xp);
+            await existingEntry.save();
+            res.json({ message: 'XP actualizado', entry: existingEntry });
+        } else {
+            const newEntry = new Ranking({ name: 'Anónimo', xp, date });
+            await newEntry.save();
+            res.status(201).json({ message: 'XP almacenado', entry: newEntry });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
 // Endpoint para obtener el ranking del día
 app.get('/ranking', async (req, res) => {
     const date = new Date().toISOString().split('T')[0];
@@ -42,21 +81,14 @@ app.get('/ranking', async (req, res) => {
 
 // Endpoint para agregar una nueva entrada al ranking
 app.post('/ranking', async (req, res) => {
-    const { name, xp } = req.body;
-    const date = new Date().toISOString().split('T')[0];
-
-    const newEntry = new Ranking({ name, xp, date });
+    const { name, xp, date } = req.body;
     try {
+        const newEntry = new Ranking({ name, xp, date });
         await newEntry.save();
         res.status(201).json({ message: 'Ranking actualizado', entry: newEntry });
     } catch (err) {
         res.status(500).send(err);
     }
-});
-
-// Agregar un Endpoint para la Ruta Raíz
-app.get('/', (req, res) => {
-    res.send('Bienvenido a El Codi Game API. Utiliza los endpoints /ranking para obtener información.');
 });
 
 // Iniciar el servidor
